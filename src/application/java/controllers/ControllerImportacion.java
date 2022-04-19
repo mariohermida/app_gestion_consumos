35ed2c376453d5c995e85a6a248048dae990a64f
Mario Hermida <marioher98@gmail.com>
2022-04-19 10:22:55 +0200

    Welcome message and entity names changed.

--- src/application/java/controllers/ControllerImportacion.java
+++ src/application/java/controllers/ControllerImportacion.java
@@ -1,9 +1,13 @@
 package application.java.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Properties;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
@@ -18,6 +22,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
@@ -25,12 +30,40 @@ import javafx.stage.FileChooser;
  * Class that manages all the events occurred in Importacion.fxml
  */
 public class ControllerImportacion {
-	
+
 	@FXML
 	private AnchorPane rootPane;
 
 	@FXML
-	void importarAplicaciones(ActionEvent event) throws IOException, CsvValidationException {
+	private Button importarEntity1Button;
+
+	@FXML
+	private Button importarEntity2Button;
+
+	@FXML
+	private Button importarEntity3Button;
+
+	// Object for retrieving the values stored in file
+	private Properties properties = new Properties();
+
+	@FXML
+	public void initialize() {
+		// Title name for buttons are retrieved from file
+		try {
+			properties.load(new FileInputStream(new File("C:\\Users\\SIC-LN-34\\Desktop\\M\\titles.properties")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		importarEntity1Button.setText("Importar " + properties.getProperty("entity1"));
+		importarEntity2Button.setText("Importar " + properties.getProperty("entity2"));
+		importarEntity3Button.setText("Importar " + properties.getProperty("entity3"));
+	}
+
+	@FXML
+	void importarEntity1(ActionEvent event) throws IOException, CsvValidationException {
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Seleccione el archivo de aplicaciones a importar:");
 		File file = fc.showOpenDialog(null);
@@ -50,10 +83,10 @@ public class ControllerImportacion {
 		}
 		reader.close();
 		csvReader.close();
-	}	
+	}
 
 	@FXML
-	void importarUsuarios(ActionEvent event) throws IOException, CsvValidationException {
+	void importarEntity2(ActionEvent event) throws IOException, CsvValidationException {
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Seleccione el archivo de usuarios a importar:");
 		File file = fc.showOpenDialog(null);
@@ -76,7 +109,7 @@ public class ControllerImportacion {
 	}
 
 	@FXML
-	void importarConsumos(ActionEvent event) throws IOException, CsvValidationException {
+	void importarEntity3(ActionEvent event) throws IOException, CsvValidationException {
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Seleccione el archivo de consumos a importar:");
 		File file = fc.showOpenDialog(null);
@@ -98,7 +131,7 @@ public class ControllerImportacion {
 		reader.close();
 		csvReader.close();
 	}
-	
+
 	@FXML
 	void atras(ActionEvent event) {
 		AnchorPane pane = null;
--- src/application/java/controllers/ControllerPrincipal.java
+++ src/application/java/controllers/ControllerPrincipal.java
@@ -1,41 +1,80 @@
 package application.java.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 /**
  * Class that manages all the events occurred in Principal.fxml
  */
 public class ControllerPrincipal {
-	
+
 	@FXML
 	private AnchorPane rootPane;
 
 	@FXML
-	void aplicacionesButton(ActionEvent event) {
-		openNewWindow("Aplicaciones");
+	private Label welcomeLabel;
+
+	@FXML
+	private Button entity1Button;
+
+	@FXML
+	private Button entity2Button;
+
+	@FXML
+	private Button entity3Button;
+
+	// Object for retrieving the values stored in file
+	private Properties properties = new Properties();
+
+	@FXML
+	public void initialize() {
+		// Title name for buttons are retrieved from file
+		try {
+			properties.load(new FileInputStream(new File("C:\\Users\\SIC-LN-34\\Desktop\\M\\titles.properties")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		welcomeLabel.setText(properties.getProperty("welcome"));
+		entity1Button.setText(properties.getProperty("entity1"));
+		entity2Button.setText(properties.getProperty("entity2"));
+		entity3Button.setText(properties.getProperty("entity3"));
 	}
 
 	@FXML
-	void usuariosButton(ActionEvent event) {
-		openNewWindow("Usuarios");
+	void entity1(ActionEvent event) {
+		openNewWindow("Entity1");
 	}
 
 	@FXML
-	void consumosButton(ActionEvent event) {
-		openNewWindow("Consumos");
+	void entity2(ActionEvent event) {
+		openNewWindow("Entity2");
 	}
 
 	@FXML
-	void avanzadoButton(ActionEvent event) {
+	void entity3(ActionEvent event) {
+		openNewWindow("Entity3");
+	}
+
+	@FXML
+	void avanzado(ActionEvent event) {
 		openNewWindow("Login");
 	}
 
 	@FXML
-	void auditTrailButton(ActionEvent event) {
+	void auditTrail(ActionEvent event) {
 		openNewWindow("AuditTrail");
 	}
 
